d7d2c6d3025e49f8c64841c7f655c8766436dcad
Xiao Pan <929369239@qq.com>
2020-09-18 15:25:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Home></Home>
+</template>
+
+<script>
+import Home from './components/Home/index'
+export default {
+  name: 'App',
+  components:{
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
